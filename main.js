f08e982f4ab5b8fac91d0018aadaae3b5d106f62
Đặng Linh <104380101+linhdepzai@users.noreply.github.com>
2022-12-01 22:58:28 +0700

    load model

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-// import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-console.log(scene)
-const loader = new GLTFLoader();
+let houseMesh;
+let light;
 
-loader.load( 'path/to/model.glb', function ( gltf ) {
+function init(){
+	scene.background = new THREE.Color('green');
+	camera.position.set(0, 1, 2);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+}
 
-	scene.add( gltf.scene );
+function setLight(){
+	light = new THREE.AmbientLight(0xffffff);
+	scene.add(light);
+}
 
-}, undefined, function ( error ) {
+function loadGLTF(){
+	const  houseLoader = new GLTFLoader();
+	houseLoader.load('/models/shapeModel.glb', (gltf) => {
+		houseMesh = gltf.scene;
+		houseMesh.scale.set(0.2, 0.2, 0.2);
+		scene.add(houseMesh);
+		houseMesh.position.set(0, 0, 0);
+	});
+}
 
-	console.error( error );
+function animate(){
+	requestAnimationFrame(animate);
+	if(houseMesh && houseMesh.rotation){
+		houseMesh.rotation.y -= 0.005;
+	}
+	renderer.render(scene, camera);
+}
 
-} );
+init();
+setLight();
+loadGLTF();
+animate();
